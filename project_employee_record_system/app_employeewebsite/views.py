3e2299935045f300c9c4e94bce76aa70118f8b94
Bipashh <114556150+Bipashh@users.noreply.github.com>
2024-06-09 12:07:24 +0000

    models and urls

--- project_employee_record_system/app_employeewebsite/views.py
+++ project_employee_record_system/app_employeewebsite/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
-def demo(request):
-    context = {
-        "class_name": "Django Class"
-    }
-    return render(request, "demo.html", context)
+def employee_add(request):
+    # context = {
+    #     "class_name": "Django Class"
+    # }
+    # return render(request, "demo.html", context)
+    return render(request, "employees/add_employees.html")
+def employee_edit(request):
+    return render(request, "employees/edit_employees.html")
+def employee_index(request):
+    return render(request, "employees/index_employees.html")
+def employee_show(request):
+    return render(request, "employees/show_employees.html")
